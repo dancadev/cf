25f189a784dd01dc5af4e81697e05531926d5915
dancadev <dancadev@gmail.com>
2023-03-11 14:05:51 -0500

    Condicion if[F

---
+++ javascript/condicion.js
@@ -0,0 +1,19 @@
+if(10) console.log("10 es verdadero") //si tansolo es una condicion se usa en una sola linea
+//tansolo se ejecuta si la condicion es true
+let edad= 14
+if(edad>=18){
+    console.log("es mayor de edad")
+    console.log(":D")
+}else{
+    console.log("Es menor de edad")
+}
+
+let calificacion = 9
+if(calificacion === 10){
+    console.log("Tu calificación fue: "+calificacion + " Excelente!")
+}else if(calificacion >= 9 && calificacion<10){
+    console.log("Tiene una calificacion de: " + calificacion + " Su calificación es Buena")
+}else{
+    console.log('Puede mejorar')
+}
+
