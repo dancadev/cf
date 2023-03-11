b4fe59361c1157b7c48e268c1d0ede0dea3798e3
dancadev <dancadev@gmail.com>
2023-03-11 14:42:53 -0500

    Agregando contenido de ciclos for while dowhile

---
+++ javascript/ciclos.js
@@ -0,0 +1,28 @@
+//imprimir numeros del 1 al 10
+/*
+1. instruccion inciial
+2. Condicion 
+3. Instruccion despues de cada iteracion
+
+*/
+
+/*
+for (let i = 1;i<=10;i+=1){
+    console.log(i)
+    //if (i==5){break} llega hasta 5 y termina el ciclo
+    if (i % 2 != 0){continue}
+    console.log(i+' Es par')
+}*/
+/*
+let u = 1
+while(u <= 10){
+    console.log(u)
+   
+    u+=1
+}*/
+
+let u = 1 
+do {
+    console.log(u)
+    u+=1
+} while (u <= 20)
