f5ef882d6a88e79a3e5614187640bb17ce0c287f
dancadev <dancadev@gmail.com>
2023-04-22 16:46:23 -0500

    Agragando variables y operadores

---
+++ javascript/operadores.js
@@ -0,0 +1,4 @@
+console.log(typeof(5))
+console.log(typeof(false))
+console.log(typeof('David'))
+console.log(typeof(undefined))
---
+++ javascript/variables_constantes.js
@@ -0,0 +1,11 @@
+let midato
+let dato = 5
+midato = 'ocho'
+
+console.log(midato)
+console.log(dato)
+
+let x=1
+let y = ++x
+console.log(x)
+console.log(y)
